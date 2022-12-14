c3d709d91fccd090a3782519daa8d4bcb9041892
Ben Jones <58306801+Benjonesmtb@users.noreply.github.com>
2022-12-14 19:33:58 +0000

    Update data.py

--- data.py
+++ data.py
@@ -8,14 +8,15 @@ def create():
   cursor = conn.cursor()
 
 # Create the "users" table
-  cursor.execute('CREATE TABLE users (id TEXT, username TEXT, password TEXT, score TEXT)')
+  cursor.execute('CREATE TABLE users (id TEXT, username TEXT, password TEXT, score NUMBER)')
 
 # Add some users to the database
-  cursor.execute('INSERT INTO users VALUES ("1", "johndoe", "secret", "0")')
-  cursor.execute('INSERT INTO users VALUES ("2", "janedoe", "password", "0")')
-  cursor.execute('INSERT INTO users VALUES ("3", "user1", "pass1", "0")')
-  cursor.execute('INSERT INTO users VALUES ("4", "user2", "pass2", "0")')
-  cursor.execute('INSERT INTO users VALUES ("5", "user3", "pass3", "0")')
+  cursor.execute('INSERT INTO users VALUES ("1", "johndoe", "secret", 1)')
+  cursor.execute('INSERT INTO users VALUES ("2", "janedoe", "password", 2)')
+  cursor.execute('INSERT INTO users VALUES ("3", "user1", "pass1", 36)')
+  cursor.execute('INSERT INTO users VALUES ("4", "user2", "pass2", 4)')
+  cursor.execute('INSERT INTO users VALUES ("5", "user3", "pass3", 8)')
+  cursor.execute('INSERT INTO users VALUES ("6", "user4", "pass4", 2)')
   print(1)
 
 # Save the changes
